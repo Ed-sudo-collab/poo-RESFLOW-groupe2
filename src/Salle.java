5eb788ab9d404fd6387bd0ec3925e22da71994fb
eldineminoungou48-arch <eldineminoungou48@gmail.com>
2026-09-12 21:44:34 +0800

    Ajout la classe Salle avec capacite

---
+++ src/Salle.java
@@ -0,0 +1,12 @@
+public class Salle extends Ressource {
+
+    private int capacite;
+
+    public Salle(int numero, String nom, int capacite) {
+        super(numero, nom);
+        this.capacite = capacite;
+    }
+
+    
+
+}
