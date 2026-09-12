7d1abe9b9304dc3fbbd51d9d117a73fe49f26af0
KOARA Dorianne <dorianekoara@gmail.com>
2026-09-12 22:09:57 +0000

    Ajoute le controle de duree dans reserver

--- src/Ressource.java
+++ src/Ressource.java
@@ -32,7 +32,31 @@ public abstract class Ressource {
     }
 
     public boolean reserver(int numeroReservation, Utilisateur utilisateur, Creneau creneau) {
-       //à compléter
+        int duree;
+
+        // 1. Ressource déjà prise ?
+        if (!disponible) {
+            System.out.println("Reservation refusee : " + nom + " est deja reservee.");
+            return false;
+        }
+
+        // 2. Duree du creneau (en heures)
+        duree = creneau.getHeureFin() - creneau.getHeureDebut();
+
+        // 3. B5 : refus si la duree depasse le maximum de CETTE ressource
+        if (duree > dureeMaxReservation()) {
+            System.out.println(
+                    "Reservation refusee : le creneau dure " + duree
+                            + " heure(s), alors que " + nom + " accepte au maximum "
+                            + dureeMaxReservation() + " heure(s).");
+            return false;
+        }
+
+        // 4. Sinon, reservation acceptee
+        reservationActive = new Reservation(numeroReservation, this, utilisateur, creneau);
+        disponible = false;
+        System.out.println("Reservation acceptee.");
+        return true;
     }
 
     public void liberer() {
